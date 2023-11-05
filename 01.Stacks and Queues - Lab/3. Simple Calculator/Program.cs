cfe88bc8ffe918d7b19de584aaeadd555b6605fa
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2023-11-06 00:04:29 +0200

    Added new file directory for Stacks and Queues - Lab

---
+++ 01.Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            int result = int.Parse(stack.Pop());
+            while (stack.Count > 0)
+            {
+                string sign = stack.Pop();
+                int number = int.Parse(stack.Pop());
+
+                if (sign == "+")
+                {
+                    result += number;
+                }
+                else if (sign == "-")
+                {
+                    result -= number;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01.Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+
+            string command = Console.ReadLine();
+            int currentCountOfCustomers = 0;
+
+            while (command != "End")
+            {
+                if (command == "Paid" && customers.Any())
+                {
+                    Console.WriteLine($"{string.Join(Environment.NewLine, customers)}");
+                    for (int i = 0; i < currentCountOfCustomers; i++)
+                    {
+                        customers.Dequeue();
+                    }
+                    currentCountOfCustomers = 0;
+                }
+                else
+                {
+                    customers.Enqueue(command);
+                    currentCountOfCustomers++;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
