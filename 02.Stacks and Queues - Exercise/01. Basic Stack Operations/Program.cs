d3c0f093e60862e506875b47c6aff2e7924361c3
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2023-11-06 00:04:55 +0200

    Added new file directory for Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int elementsToRemove = numbers[1];
+            int elementToLookFor = numbers[2];
+
+            for (int i = 0; i < elementsToRemove; i++)
+            {
+                stack.Pop();
+            }
+
+            if (!stack.Any())
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (stack.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
---
+++ 02.Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //80 out of 100 points in Judge. You can try to solve the problem and find my error
+
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new(
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                quantityOfFood -= orders.Peek();
+
+                if (quantityOfFood < 0)
+                {
+                    break;
+                }
+
+                orders.Dequeue();
+            }
+            if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(", ", orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
