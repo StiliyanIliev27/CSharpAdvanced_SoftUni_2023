cfae7d46c628115256451150aeaf8bf1eee84c36
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2023-11-08 23:55:55 +0200

    Added the second half of the exercises from 04.Multidimensional Arrays - Exercise to C# Advanced - Course SoftUni

---
+++ 04.Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _5._Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = sizeInfo[0];
+            int cols = sizeInfo[1];
+            char[,] matrix = new char[rows, cols];
+
+            string text = Console.ReadLine();
+            int currentWordIndex = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (currentWordIndex == text.Length)
+                        {
+                            currentWordIndex = 0;
+                        }
+                        matrix[row, col] = text[currentWordIndex];
+                        currentWordIndex++;
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        if (currentWordIndex == text.Length)
+                        {
+                            currentWordIndex = 0;
+                        }
+                        matrix[row, col] = text[currentWordIndex];
+                        currentWordIndex++;
+                    }
+                }
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04.Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _6._Jagged_Array_Manipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] cols = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jagged[row] = cols;
+            }
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] *= 2;
+                        jagged[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jagged[row + 1].Length; col++)
+                    {
+                        jagged[row + 1][col] /= 2;
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] commandInfo = command.Split();
+                string commandType = commandInfo[0];
+                int row = int.Parse(commandInfo[1]);
+                int col = int.Parse(commandInfo[2]);
+                int value = int.Parse(commandInfo[3]);
+
+                if (row >= 0 && row < jagged.Length && col >= 0 && col < jagged[row].Length)
+                {
+                    if (commandType == "Add")
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else if (commandType == "Subtract")
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write($"{jagged[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
